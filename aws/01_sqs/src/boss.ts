712e71a42f5beedd27df9b15c14a0083dc4aa28b
Lucas Bonomo <bonomo.lucas@gmail.com>
2022-11-28 00:53:00 -0300

    Improve worker

--- aws/01_sqs/src/boss.ts
+++ aws/01_sqs/src/boss.ts
@@ -1,5 +1,15 @@
-var AWS = require("aws-sdk");
-var credentials = new AWS.SharedIniFileCredentials({ profile: 'queue' });
+import AWS from 'aws-sdk';
+import * as dotenv from 'dotenv'
+import {nanoid} from 'nanoid';
+
+// Environment vars
+dotenv.config()
+let queue_url:string = ''
+if ( process.env.QUEUE_URL ) {
+    queue_url = process.env.QUEUE_URL
+}
+
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'queue' });
 AWS.config.credentials = credentials
 
 if (AWS.config.credentials.accessKeyId == undefined) {
@@ -10,35 +20,55 @@ if (AWS.config.credentials.accessKeyId == undefined) {
 // Set the region ???
 AWS.config.update({ region: 'us-east-1' });
 
-// Create an SQS service object
-var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
-let data = {
-    uri: {
-        base: "https://example.com/",
-        target: "https://stage.example.com/"
-    },
-    render: {
-        cookie_selector: "a#hs-eu-confirmation-button",
-        exclude: [
-            "#masthead",
-            ".site-footer"
-        ],
-        include: "main"
-    },
-    auth: {
-        type: "basic",
-        username: "admin",
-        password: "adminPass"
+/**
+ * Get messages form Bacon Ipsum.
+ * 
+ * @returns Messages
+ */
+async function getLorem() {
+    let messages:string[] = []
+    const url = 'https://baconipsum.com/api/?type=all-meat'
+
+    // Make a request for a user with a given ID
+    let response = await fetch(url)
+    if (response.ok) {
+        messages = await response.json();
+    } else {
+        messages = []
+    }
+    
+    return messages
+}
+
+
+/**
+ * Main function.
+ */
+async function main() {
+
+    const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+    const messages = await getLorem()
+    for (let message in messages) {
+        
+        let data = {
+            id: nanoid(9),
+            datetime: Date.now(),
+            message: messages[message]
+        }
+        
+        const params = {
+            DelaySeconds: 10,
+            MessageBody: JSON.stringify(data),
+            QueueUrl: queue_url
+        };
+
+        sqs.sendMessage(params, function (err: any, data: any) {
+            console.log(`Added message: ${data.MessageId}`)
+            if (err) {
+                console.log(`Error (message: ${data.MessageId}), ${err}`);
+            }
+        });
     }
 }
-var params1 = {
-    DelaySeconds: 10,
-    MessageBody: JSON.stringify(data),
-    QueueUrl: "https://sqs.us-east-1.amazonaws.com/024857090340/terraform-example-queue"
-};
 
-sqs.sendMessage(params1, function (err: any, data: any) {
-    if (err) {
-        console.log(`Error (message: ${data.MessageId}), ${err}`);
-    }
-});
+main()
