6302d5f192e867d25ff72728cf5279686f536b39
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-08-04 18:52:39 -0300

    Lambda

--- aws/06_lambda/src/index.ts
+++ aws/06_lambda/src/index.ts
@@ -3,11 +3,32 @@ import 'dotenv/config'
 const words = process.env.WORDS.split(',')
 
 exports.handler = async (event: any, context: any) => {
-    const random = Math.floor(Math.random() * words.length)
-    let jsonResponse = { 'word': words[random] }
     const response = {
         statusCode: 200,
-        body: JSON.stringify(jsonResponse),
+        body: JSON.stringify({
+            'path': event.path,
+            'event': event,
+            'context':context
+        }),
     };
     return response
+    // if ( context.path !== "/" ) {
+    //     let jsonResponse = { 'resource': context.path }
+    //     const response = {
+    //         statusCode: 200,
+    //         body: JSON.stringify(jsonResponse),
+    //     };
+    //     return response
+    // } else {
+
+
+    // }
+
+//     const random = Math.floor(Math.random() * words.length)
+//     let jsonResponse = { 'word': words[random] }
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify(jsonResponse),
+//     };
+//     return response
 }
