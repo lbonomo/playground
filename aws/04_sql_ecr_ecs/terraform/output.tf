fd2e811ce16858852caef0997e1f5dd2595404a0
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-09-14 07:44:49 -0300

    Sorting...

--- aws/04_sql_ecr_ecs/terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "sqs_url_data_source" {
-  value = aws_sqs_queue.example-queue.url
-}
--- aws/04_sql_ecr_ecs/terraform/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source = "hashicorp/aws"
-    }
-  }
-}
