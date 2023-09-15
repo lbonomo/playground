5ef327c7151ccab3056c1e7041a9f5649f7e9fa9
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-09-15 19:51:42 -0300

    S3 public

--- aws/08_S3/s3.tf
+++ aws/08_S3/s3.tf
@@ -10,32 +10,37 @@ resource "aws_s3_bucket" "plugins" {
   }
 }
 
-# resource "aws_s3_bucket_public_access_block" "plugins" {
-#   bucket = aws_s3_bucket.plugins.id
-  
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_public_access_block" "plugins" {
+  bucket = aws_s3_bucket.plugins.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-# resource "aws_s3_bucket_policy" "plugins" {
-#   bucket = var.bucket_id
-#   policy = data.aws_iam_policy_document.plugins.json
-# }
 
-# data "aws_iam_policy_document" "plugins" {
-#   version = "2008-10-17"
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.plugins.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
 
-#   statement {
-#     actions   = ["s3:ListBucket"]
-#     resources = [var.bucket_arn]
+# 
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
 
-#     principals {
-#       type = "AWS"
-#       identifiers = [
-#         aws_cloudfront_origin_access_identity.this.iam_arn
-#       ]
-#     }
-#   }
-# }
+    principals {
+	    type = "*"
+	    identifiers = ["*"]
+	  }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.plugins.arn,
+      "${aws_s3_bucket.plugins.arn}/*",
+    ]
+  }
+}
