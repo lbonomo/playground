1d5301d31f63ef84b9d957a275b569a5f8282ecb
Lucas Bonomo <bonomo.lucas@gmail.com>
2023-11-14 10:32:19 -0300

    Install script first approach

--- gcp/compute_engine/main.tf
+++ gcp/compute_engine/main.tf
@@ -36,19 +36,19 @@ resource "google_compute_subnetwork" "default" {
 # Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
   # Este puede ser el la unica variable por reposigorio
-  name         = "runner-cliente1"
-  machine_type = "f1-micro"
-  zone         = "us-west1-a"
-  tags         = ["terrafrom", "ssh"]
-
+  name = "runner-cliente1"
+  # gcloud compute machine-types list --project=[project-name] --filter=guestCpus=8
+  machine_type              = "c2d-highmem-8"
+  zone                      = "us-west1-a"
+  tags                      = ["terrafrom", "ssh"]
+  allow_stopping_for_update = true # neccesary to change machine_type
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
   }
-
-  # Install runner
-  # metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install runner"
+   
+  metadata_startup_script = "${file("./install.sh")}"
 
   network_interface {
     subnetwork = google_compute_subnetwork.default.id
