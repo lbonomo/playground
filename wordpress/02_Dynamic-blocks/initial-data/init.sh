db52e4167ebb99f861a6719685d06aef50491e71
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-11-21 15:52:53 -0300

    Improvement init script

--- wordpress/02_Dynamic-blocks/initial-data/init.sh
+++ wordpress/02_Dynamic-blocks/initial-data/init.sh
@@ -5,12 +5,15 @@ size=${#posts[@]}
 # if there is just one post, import pages and posts.
 if [[ $size -lt 5 ]]; then
 	# Remove previus pages and post.
-	for id in $posts; do
-		wp-env run cli wp post delete $id --force
+	index=0
+	while [ $index -lt $size ];	do
+		wp-env run cli wp post delete ${posts[$index]} --force
+		((index++))
 	done
-
+	# Import pages and posts.
 	wp-env run cli wp import --authors=create ./initial-data/pages.xml
 	wp-env run cli wp import --authors=create ./initial-data/posts.xml
+	# Set home page.
 	frontpage=$(wp-env run cli wp post list --post_type='page' --post_status='publish' --format=ids)
 	wp-env run cli wp option update show_on_front page
 	wp-env run cli wp option update page_on_front $frontpage
